f2c2b61b2a6c7cc293fe28372fbdec505a06ae3d
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-25 09:25:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const PREFIX = "_";
 
-client.on("guildCreate", guild => {
-  // This event triggers when the bot joins a guild.
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`With TaMoToJi`);
 });
 
 var name;
